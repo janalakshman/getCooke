78fe5138c3eba225922ef1ab5a220ac7f6131050
Jana Lakshman <janalakshman27@gmail.com>
2021-04-06 18:32:42 +0530

    UI

--- Home.js
+++ Home.js
@@ -99,7 +99,7 @@ export default function Home( {navigation} ) {
         return (
           <ScrollView>
             <Title name={recipe} />
-            <View style={{paddingLeft : 64, paddingVertical : 8, marginTop:8, backgroundColor : '#fff5e6'}}>
+            <View style={{paddingLeft : 16, paddingVertical : 8, marginTop:8, backgroundColor : '#fff5e6'}}>
             <FlatList
               data={recipes[recipe]}
               renderItem={renderCard}
